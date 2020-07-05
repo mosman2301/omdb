5e01953825649273a5e4e211552b122278db2b7d
louisatu <louisatu@umn.edu>
2020-07-05 14:12:10 -0500

    report commit
    reports.php added and nav.php updated to link to reports page

--- reports.php
+++ reports.php
@@ -4,14 +4,46 @@
   $left_selected = "";
 
   include("./nav.php");
+  global $db;
   
  ?>
 
  <div class="right-content">
     <div class="container">
 
-      <h3 style = "color: #01B0F1;">Reports (TO BE DONE LATER)</h3>
+      <table id="info" cellpadding="0" cellspacing="0" border="0"
+            class="datatable table table-striped table-bordered datatable-style table-hover"
+            width="100%" style="width: 200px;">
+              <thead>
+                <tr id="table-first-row">
+                        <th>Year Made</th>
+                        <th>Number of movies</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  $sql = "SELECT DISTINCT year_made, COUNT(*) as 'number of movies'FROM movies GROUP BY year_made;";
+                  $result = $db->query($sql);
+                  if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo '<tr>
+                                <td>'.$row["year_made"].'</td>
+                                <td>'.$row["number of movies"].'</td>
+                            </tr>';
+                    }//end while
+                }//end if
+                else {
+                    echo "0 results";
+                }//end else
 
+                 $result->close();
+                ?>
+              </tbody>
+      </table>
+
+
+    
     </div>
 </div>
 
