f00f0d96f1e7c9d767c5cc321e3ea01bc3d2e5c8
abdiaziz <43540550+abdiaziz@users.noreply.github.com>
2020-07-13 21:20:53 -0500

    Update and rename omdb_sql_queries iter 8.sql to omdb_sql_queries.sql

--- sql/omdb_sql_queries iter 8.sql
+++
@@ -1,8 +0,0 @@
-/*!
-By: Aziz Moalim
-7.43
-Give me a summary of m_link_type. The result set should contain the count of each m_link_type. */;
-
-SELECT m_link_type, count(*)
-FROM movie_media
-GROUP BY m_link_type;
